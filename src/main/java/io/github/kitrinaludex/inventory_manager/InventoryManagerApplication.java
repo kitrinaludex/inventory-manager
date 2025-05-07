ef50ba71f275e0b51e16b81772587011d6d61613
Cbtdeus <cbtdeus@proton.me>
2025-05-07 22:06:42 +0300

    initial commit

---
+++ src/main/java/io/github/kitrinaludex/inventory_manager/InventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.kitrinaludex.inventory_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerApplication.class, args);
+	}
+
+}
