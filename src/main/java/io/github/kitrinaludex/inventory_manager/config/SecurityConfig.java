cbd85760cbb54cfc08708d73ebfd27460b335a65
Cbtdeus <cbtdeus@proton.me>
2025-05-28 16:56:55 +0300

    added a filter

--- src/main/java/io/github/kitrinaludex/inventory_manager/config/SecurityConfig.java
+++ src/main/java/io/github/kitrinaludex/inventory_manager/config/SecurityConfig.java
@@ -2,13 +2,30 @@ package io.github.kitrinaludex.inventory_manager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.web.SecurityFilterChain;
+@EnableWebSecurity(debug = true)
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.httpBasic(Customizer.withDefaults());
+         http.authorizeHttpRequests(auth -> auth
+                         .requestMatchers("/register").permitAll()
+                         .requestMatchers("/inventory/*").authenticated()
+                        );
+
+
+        return http.build();
+    }
 
 
     @Bean
